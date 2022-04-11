bfd650cc4987d17813c66fc32db7cf6f06e9ace7
joyLASTnight <anggara.lucky15@gmail.com>
2022-04-11 22:14:39 +0700

    asdasdasd

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '',
+  base: '/sibaper_frontend/',
+
   plugins: [vue()],
-  resolve: {
-    alias: [
-      {
-        find: /^@\/(.+)/,
-        replacement: path.resolve(__dirname, 'src') + '/$1',
-      },
-    ],
-  },
 })
